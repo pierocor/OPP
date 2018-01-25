6dff293af73db5163a4f300e86e3860854a8cbf0
rajat <rjtkpanda@gmail.com>
2018-01-25 12:15:53 +0100

    added velocity verlet header

---
+++ include/velocity_verlet.h
@@ -0,0 +1,9 @@
+#ifndef VELOCITY_VERLET_H
+#define VELOCITY_VERLET_H
+
+/*the list of functions.*/
+static void velverlet1(mdsys_t *sys)
+static void velverlet2(mdsys_t *sys)
+static void force(mdsys_t *sys)
+
+#endif
