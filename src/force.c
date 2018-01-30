c45ec96db79dac947f4d224cbe6a71b75215815e
auroramaurizio <auroramaurizio1@gmail.com>
2018-01-30 16:20:35 +0100

    moved static pbc in force

--- src/force.c
+++ src/force.c
@@ -7,6 +7,13 @@
 #define TAG 50
 #endif
 
+/* helper function: apply minimum image convention */
+static double pbc(double x, const double boxby2)
+{
+    while (x >  boxby2) x -= 2.0*boxby2;
+    while (x < -boxby2) x += 2.0*boxby2;
+    return x;
+}
 
 /* compute forces */
 
