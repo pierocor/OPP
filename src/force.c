2df65d16dd0dca4db7a90523f62e94918eab9b75
auroramaurizio <auroramaurizio1@gmail.com>
2018-01-30 16:30:19 +0100

    moved pbc in forces

--- src/force.c
+++ src/force.c
@@ -2,6 +2,13 @@
 #include <data.h>
 #include <velocity_verlet.h>
 
+/* helper function: apply minimum image convention */
+static double pbc(double x, const double boxby2)
+{
+    while (x >  boxby2) x -= 2.0*boxby2;
+    while (x < -boxby2) x += 2.0*boxby2;
+    return x;
+}
 
 /* compute forces */
 void force(mdsys_t *sys)   /*AAA*/
